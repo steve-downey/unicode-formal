262d2501d62978f00d1ead7d16267fbe18229e21
Steve Downey <sdowney2@bloomberg.net>
2023-09-01 10:13:35 -0400

    De-scratch the scratch copy
    Convert to Unicode-Formal

--- src/examples/main.cpp
+++ src/examples/main.cpp
@@ -1,10 +1,10 @@
-#include <scratch/scratch.h>
+#include <unicode_formal/unicode_formal.h>
 
 #include <iostream>
 
 int main(int /*argc*/, char** /*argv*/)
 {
-    using namespace scratch;
+    using namespace unicode_formal;
 
 
 }
