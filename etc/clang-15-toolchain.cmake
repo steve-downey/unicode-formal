a97e826cd3e49c60171a8bb416550e82a5efda92
Steve Downey <sdowney2@bloomberg.net>
2023-10-02 23:23:29 -0400

    additional chains

--- etc/clang-15-toolchain.cmake
+++ etc/clang-15-toolchain.cmake
@@ -1,7 +1,7 @@
 include_guard(GLOBAL)
 
-set(CMAKE_C_COMPILER clang-16)
-set(CMAKE_CXX_COMPILER clang++-16)
+set(CMAKE_C_COMPILER clang-15)
+set(CMAKE_CXX_COMPILER clang++-15)
 
 set(CMAKE_CXX_FLAGS
   "-std=c++20 \
--- etc/clang-16-toolchain.cmake
+++ etc/clang-16-toolchain.cmake
@@ -1,14 +1,12 @@
 include_guard(GLOBAL)
 
-set(CMAKE_CXX_STANDARD 20)
-
 set(CMAKE_C_COMPILER clang-16)
 set(CMAKE_CXX_COMPILER clang++-16)
 
 set(CMAKE_CXX_FLAGS
   "-std=c++20 \
    -Wall -Wextra \
-   -stdlib=libc++ "
+   -stdlib=libc++"
 CACHE STRING "CXX_FLAGS" FORCE)
 
 set(CMAKE_CXX_FLAGS_DEBUG "-O0 -fno-inline -g3" CACHE STRING "C++ DEBUG Flags" FORCE)
